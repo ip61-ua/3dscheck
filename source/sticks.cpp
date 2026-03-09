c3c1dd5d738a072f98a54d1a31a88568d0d2b319
ip61-ua <ip61@alu.ua.es>
2026-03-09 09:06:48 +0100

    feat: make relative stick

--- source/sticks.cpp
+++ source/sticks.cpp
@@ -1,56 +1,52 @@
-#include <citro2d.h>
-#include <3ds.h>
 #include "extra.h"
 #include "main.h"
+#include <3ds.h>
+#include <citro2d.h>
 
-void drawLStick(int posX, int posY) {
-    u32 white = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
+void
+drawLStick (int posX, int posY)
+{
+  u32 white = C2D_Color32 (0xFF, 0xFF, 0xFF, 0xFF);
 
-    hidScanInput();
-    u32 kDown = hidKeysDown();
-    circlePosition pos;
-    hidCircleRead(&pos);
-    if(kDown & KEY_B)
-        mainMenuSet();
+  hidScanInput ();
+  u32 kDown = hidKeysDown ();
+  circlePosition pos;
+  hidCircleRead (&pos);
+  if (kDown & KEY_B)
+    mainMenuSet ();
 
+  m_useColor (0xFF, 0xFF, 0xFF);
+  m_circle (165, 85, 70);
+  m_useColor (0x35, 0x3E, 0x4A);
+  m_circle (167, 87, 66);
 
-    m_useColor(0xFF, 0xFF, 0xFF);
-    m_circle(165, 85, 70);
-    m_useColor(0x35, 0x3E, 0x4A);
-    m_circle(167, 87, 66);
-
-    //400*240
-    C2D_DrawLine(180 + pos.dx / 5, 120 + pos.dy / -5, white, 220 + pos.dx / 5, 120 + pos.dy / -5, white, 2, 0);
-    C2D_DrawLine(200 + pos.dx / 5, 100 + pos.dy / -5, white, 200 + pos.dx / 5, 140 + pos.dy / -5, white, 2, 0);
+  // 400*240
+  C2D_DrawLine (180 + pos.dx / 5, 120 + pos.dy / -5, white, 220 + pos.dx / 5,
+                120 + pos.dy / -5, white, 2, 0);
+  C2D_DrawLine (200 + pos.dx / 5, 100 + pos.dy / -5, white, 200 + pos.dx / 5,
+                140 + pos.dy / -5, white, 2, 0);
 }
 
-void drawCStick() {
-    u32 white = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
-    int posdx = 0;
-    int posdy = 0;
+void
+drawCStick ()
+{
+  u32 white = C2D_Color32 (0xFF, 0xFF, 0xFF, 0xFF);
 
-    hidScanInput();
-    u32 kHeld = hidKeysHeld();
-    if (kHeld & KEY_CSTICK_UP)
-        posdy = 120;
-    if (kHeld & KEY_CSTICK_DOWN)
-        posdy = -120;
-    if (kHeld & KEY_CSTICK_LEFT)
-        posdx = -120;
-    if (kHeld & KEY_CSTICK_RIGHT)
-        posdx = 120;
-    
+  hidScanInput ();
+  u32 kDown = hidKeysDown ();
+  circlePosition pos;
+  hidCstickRead (&pos);
+  if (kDown & KEY_B)
+    mainMenuSet ();
 
-    if(kHeld & KEY_B)
-        mainMenuSet();
+  m_useColor (0xFF, 0xFF, 0xFF);
+  m_circle (165, 85, 70);
+  m_useColor (0x35, 0x3E, 0x4A);
+  m_circle (167, 87, 66);
 
-
-    m_useColor(0xFF, 0xFF, 0xFF);
-    m_circle(165, 85, 70);
-    m_useColor(0x35, 0x3E, 0x4A);
-    m_circle(167, 87, 66);
-
-    //400*240
-    C2D_DrawLine(180 + posdx / 5, 120 + posdy / -5, white, 220 + posdx / 5, 120 + posdy / -5, white, 2, 0);
-    C2D_DrawLine(200 + posdx / 5, 100 + posdy / -5, white, 200 + posdx / 5, 140 + posdy / -5, white, 2, 0);
+  // 400*240
+  C2D_DrawLine (180 + pos.dx / 5, 120 + pos.dy / -5, white, 220 + pos.dx / 5,
+                120 + pos.dy / -5, white, 2, 0);
+  C2D_DrawLine (200 + pos.dx / 5, 100 + pos.dy / -5, white, 200 + pos.dx / 5,
+                140 + pos.dy / -5, white, 2, 0);
 }
