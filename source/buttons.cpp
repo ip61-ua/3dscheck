c0f1da8153222843b4ebd3fcd2c53e074949f256
ip61-ua <ip61@alu.ua.es>
2026-03-09 09:39:17 +0100

    refactor: buttons to macros

--- source/buttons.cpp
+++ source/buttons.cpp
@@ -1,44 +1,39 @@
-#include <citro2d.h>
-#include <3ds.h>
 #include "extra.h"
+#include <3ds.h>
+#include <citro2d.h>
 
-void checkButtons(bool n3ds = false)
+#define checkButtonsExpand(keys, key, x1, y1, x2, y2)                         \
+  {                                                                           \
+    (keys & key) ? m_useColor (0x00, 0xFF, 0x00)                              \
+                 : m_useColor (0xFF, 0x00, 0x00);                             \
+    m_rect (x1, y1, x2, y2);                                                  \
+  }
+
+void
+checkButtons (bool n3ds = false)
 {
-    hidScanInput();
-    u32 keys = hidKeysHeld();
+  hidScanInput ();
+  u32 keys = hidKeysHeld ();
 
-    if (keys & KEY_A){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(325, 110, 355, 130);
-    if (keys & KEY_Y){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(265, 110, 295, 130);
-    if (keys & KEY_X){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(295, 90, 325, 110);
-    if (keys & KEY_B){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(295, 130, 325, 150);
+  checkButtonsExpand (keys, KEY_A, 325, 110, 355, 130);
+  checkButtonsExpand (keys, KEY_Y, 265, 110, 295, 130);
+  checkButtonsExpand (keys, KEY_X, 295, 90, 325, 110);
+  checkButtonsExpand (keys, KEY_B, 295, 130, 325, 150);
 
-    if (keys & KEY_DRIGHT){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(105, 110, 135, 130);
-    if (keys & KEY_DLEFT){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(45, 110, 75, 130);
-    if (keys & KEY_DUP){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(75, 90, 105, 110);
-    if (keys & KEY_DDOWN){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(75, 130, 105, 150);
+  checkButtonsExpand (keys, KEY_DRIGHT, 105, 110, 135, 130);
+  checkButtonsExpand (keys, KEY_DLEFT, 45, 110, 75, 130);
+  checkButtonsExpand (keys, KEY_DUP, 75, 90, 105, 110);
+  checkButtonsExpand (keys, KEY_DDOWN, 75, 130, 105, 150);
 
-    if (keys & KEY_SELECT){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(100, 190, 160, 210);
-    if (keys & KEY_START){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(240, 190, 300, 210);
+  checkButtonsExpand (keys, KEY_SELECT, 100, 190, 160, 210);
+  checkButtonsExpand (keys, KEY_START, 240, 190, 300, 210);
 
-    if (keys & KEY_L){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(50, 50, 110, 70);
-    if (keys & KEY_R){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-    m_rect(290, 50, 350, 70);
+  checkButtonsExpand (keys, KEY_L, 50, 50, 110, 70);
+  checkButtonsExpand (keys, KEY_R, 290, 50, 350, 70);
 
-    if (n3ds) {
-        if (keys & KEY_ZL){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-        m_rect(120, 50, 150, 70);
-        if (keys & KEY_ZR){m_useColor(0x00, 0xFF, 0x00);} else {m_useColor(0xFF, 0x00, 0x00);}
-        m_rect(250, 50, 280, 70);
+  if (n3ds)
+    {
+      checkButtonsExpand (keys, KEY_ZL, 120, 50, 150, 70);
+      checkButtonsExpand (keys, KEY_ZR, 250, 50, 280, 70);
     }
 }
